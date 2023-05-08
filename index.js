6d2d2d6cbc587386a4a8973339deaab8c6f3161a
urswilke <urs.wilke@gmail.com>
2023-05-08 17:31:21 +0200

    - rewrite to assign to global variable - use simpler example from docs

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// import * as XLSX from "xlsx";
+
 import { xlsx_to_json_array } from './readExcel.js'
 
-
-document.getElementById("file-upload").addEventListener('change', xlsx_to_json_array);
+var xlsx_data;
+// helper function to assign value to a global variable:
+async function upload_xlsx(e) {
+    xlsx_data = await xlsx_to_json_array(e);
+    console.log(xlsx_data)
+  }
+document.getElementById("file-upload").addEventListener('change', upload_xlsx);
--- readExcel.js
+++ readExcel.js
@@ -1,26 +1,8 @@
 import * as XLSX from "xlsx";
-export function xlsx_to_json_array() {
-    var reader = new FileReader();
-    reader.onload = function() {
-        var arrayBuffer = this.result,
-            array = new Uint8Array(arrayBuffer),
-            binaryString = String.fromCharCode.apply(null, array);
-
-        /* Call XLSX */
-        var workbook = XLSX.read(binaryString, {
-            type: "binary"
-        });
-
-        /* DO SOMETHING WITH workbook HERE */
-        var first_sheet_name = workbook.SheetNames[0];
-        /* Get worksheet */
-        var worksheet = workbook.Sheets[first_sheet_name];
-        console.log(XLSX.utils.sheet_to_json(worksheet, {
-            raw: true
-        }));
-        // }
-
-        // oReq.send();
-    }
-    reader.readAsArrayBuffer(this.files[0]);
-}
+export async function xlsx_to_json_array(e) {
+    // from here: https://docs.sheetjs.com/docs/demos/local/file#file-api
+    const file = e.target.files[0];
+    const data = await file.arrayBuffer();
+    const wb = XLSX.read(data);
+    return XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+  }
